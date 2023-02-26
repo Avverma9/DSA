9b2b217bc45d6831654ea3e739320c64c07383ed
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-02-27 01:26:03 +0530

    updated

--- Beginners/isAnagram.js
+++ Beginners/isAnagram.js
@@ -1,27 +1,27 @@
 var isAnagram = function(s, t) {
-    
-   // If the length of strings is not same, return false as they cannot be anagrams
-if(s.length!=t.length) return false
+    // check if lengths are equal
+    if(s.length != t.length) return false;
 
-// Initialize an object to keep track of the count of each character in both the strings
-let count={}
-let n=s.length
+    // create a count object
+    let charCount = {};
 
-// Traverse through both strings character by character and update the count of each character
-for(let i=0;i<n;i++){
-    // If the character is not already present in count object, add it with count 0
-    if(!count[s[i]]) count[s[i]]=0
-    if(!count[t[i]]) count[t[i]]=0
-    // Increment count of the character in the first string and decrement count in the second string
-    count[s[i]]++
-    count[t[i]]--
-}
+    // iterate through strings
+    let n = s.length;
+    for(let i = 0; i < n; i++) {
+        // initialize count to zero if not present in object
+        if(!charCount[s[i]]) charCount[s[i]] = 0;
+        if(!charCount[t[i]]) charCount[t[i]] = 0;
 
-// Traverse through the count object and if any count of character is not zero, return false
-for(let ch in count){
-    if(count[ch]!==0)return false
-}
-// If all counts are zero, return true as the strings are anagrams
-return true
+        // increment count for s and decrement count for t
+        charCount[s[i]]++;
+        charCount[t[i]]--;
+    }
+
+    // check if all counts are zero
+    for(let char in charCount) {
+        if(charCount[char] !== 0) return false;
+    }
+    return true;
 };
-console.log(isAnagram("ankit", "ankit")) 
+
+console.log(isAnagram("ankit", "ankit"))
